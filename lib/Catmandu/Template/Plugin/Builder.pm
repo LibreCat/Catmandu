28ca66fb67039e6be82aeec8d39a9694b57777b5
Nicolas <nicolas.steenlant@ugent.be>
2010-11-04 17:16:34 +0100

    improve Builder

--- lib/Catmandu/Template/Plugin/Builder.pm
+++ lib/Catmandu/Template/Plugin/Builder.pm
@@ -10,6 +10,7 @@ sub new {
     my $var = join '.', @vars;
     bless {
         context => $context,
+        stack => [],
         var => $var,
     }, $class;
 }
@@ -23,133 +24,127 @@ sub builder {
 }
 
 sub var {
-    $_[0]->{var};
+    my ($self, @vars) = @_;
+    my $var = $self->{var};
+    if (@vars and $var) {
+        join '.', $var, @vars;
+    } elsif (@vars) {
+        join '.', @vars;
+    } else {
+        $var;
+    }
 }
 
 sub get {
     my ($self, @vars) = @_;
-    my $var = join '.', @vars;
-    my @key = map { ($_, 0) } ( split(/\./, $self->var), split(/\./, $var) );
+    my @key = map { ($_, 0) } split(/\./, $self->var(@vars));
     $self->{context}->stash->get(\@key);
 }
 
 sub form {
-    my $attrs = ref $_[-1] eq 'HASH' ? pop @_ : {};
+    my ($self, $attr) = @_;
+    $attr ||= {};
+    $attr->{'method'} //= "post";
+    $attr->{'action'} //= "";
+    $attr->{'accept-charset'} //= "utf-8";
+    $attr = $self->_attributes($attr);
+    my $stack = $self->{stack};
+    grep /form/, @$stack and $self->_error("Can't embed form tag");
+    push @$stack, 'form';
+    "<form$attr>";
+}
+
+sub end {
     my ($self) = @_;
-
-    if ($self->{close_form_tag}) {
-        $self->{close_form_tag} = 0;
-        return "</form>";
-    } else {
-        $attrs->{action} ||= "";
-        $attrs->{method} ||= "post";
-        $attrs = $self->_join_attrs($attrs);
-        $self->{close_form_tag} = 1;
-        "<form$attrs>";
-    }
+    my $stack = $self->{stack};
+    my $tag = pop @$stack; 
+    $tag or return;
+    "</$tag>";
 }
 
-sub hidden_field {
-    my $attrs = ref $_[-1] eq 'HASH' ? pop @_ : {};
-    my ($self, @name) = @_;
-    $attrs->{type} = "hidden";
-    $attrs->{name} //= join('.', @name) or
-        $self->{context}->throw("name missing");
-    $attrs->{value} //= $self->get($attrs->{name});
-    $attrs->{name} = join '.', $self->var, $attrs->{name} if $self->var;
-    $self->tag('input', $attrs);
+sub _field {
+    my ($self, $type, @vars) = @_;
+    my $attr = ref $vars[-1] eq 'HASH' ? pop @vars : {};
+    @vars or $self->_error("Name can't be empty");
+    $attr->{type}  = $type;
+    $attr->{name}  = $self->var(@vars);
+    $attr->{value} = $self->get(@vars);
+    $self->tag('input', $attr);
 }
 
-sub password_field {
-    my $attrs = ref $_[-1] eq 'HASH' ? pop @_ : {};
-    my ($self, @name) = @_;
-    $attrs->{type} = "password";
-    $attrs->{name} //= join('.', @name) or
-        $self->{context}->throw("name missing");
-    $attrs->{value} //= $self->get($attrs->{name});
-    $attrs->{name} = join '.', $self->var, $attrs->{name} if $self->var;
-    $self->tag('input', $attrs);
+sub hidden {
+    my ($self, @vars) = @_; $self->_field('hidden', @vars);
 }
 
-sub text_field {
-    my $attrs = ref $_[-1] eq 'HASH' ? pop @_ : {};
-    my ($self, @name) = @_;
-    $attrs->{type} = "text";
-    $attrs->{name} //= join('.', @name) or
-        $self->{context}->throw("name missing");
-    $attrs->{value} //= $self->get($attrs->{name});
-    $attrs->{name} = join '.', $self->var, $attrs->{name} if $self->var;
-    $self->tag('input', $attrs);
+sub password {
+    my ($self, @vars) = @_; $self->_field('password', @vars);
+}
+
+sub text {
+    my ($self, @vars) = @_; $self->_field('text', @vars);
 }
 
 sub text_area {
-    my $attrs = ref $_[-1] eq 'HASH' ? pop @_ : {};
-    my ($self, @name) = @_;
-    $attrs->{name} //= join('.', @name) or
-        $self->{context}->throw("name missing");
-    my $value = $self->get($attrs->{name}) // "";
-    $attrs->{name} = join '.', $self->var, $attrs->{name} if $self->var;
-    $self->tag('textarea', $attrs, $value);
+    my ($self, @vars) = @_;
+    my $attr = ref $vars[-1] eq 'HASH' ? pop @vars : {};
+    @vars or $self->_error("Name can't be empty");
+    $attr->{name} = $self->var(@vars);
+    $self->tag('textarea', $self->get(@vars), $attr);
+}
+
+sub select_options {
+    my ($self, @vars) = @_;
+    my $opts = pop @vars;
+    ref $opts eq 'ARRAY' or $self->_error("Option values missing");
+    @vars or $self->_error("Name can't be empty");
+    my $value = $self->get(@vars);
+    join '', map {
+        my $pair = ref $_ eq 'ARRAY' ? $_ : [$_, $_];
+        my $attr = {value => $pair->[0]};
+        $attr->{selected} = "selected" if $value eq $pair->[0];
+        $self->tag('option', $pair->[1] // $pair->[0], $attr);
+    } @$opts;
 }
 
 sub select {
-    my $attrs = ref $_[-1] eq 'HASH' ? pop @_ : {};
-    my ($self, @name) = @_;
-    $attrs->{name} //= join('.', @name) or
-        $self->{context}->throw("name missing");
-    my $value = $self->get($attrs->{name});
-    $attrs->{name} = join '.', $self->var, $attrs->{name} if $self->var;
-    my $options = delete $attrs->{options} or 
-        $self->{context}->throw("options missing");
-    $options = join "", map {
-        my $o = $_;
-        my $v;
-        my $d;
-        if (ref $o eq 'ARRAY') {
-            $v = $o->[0];
-            $d = $o->[1] // $o->[0];
-        } else {
-            $v = $d = $o;
-        }
-        my $a = {value => $v};
-        $a->{selected} = "selected" if $v eq $value;
-        $self->tag('option', $a, $d)
-    } @$options;
-    $self->tag('select', $attrs, $options);
+    my ($self, @vars) = @_;
+    my $attr = ref $vars[-1] eq 'HASH' ? pop @vars : {};
+    my $opts = pop @vars;
+    @vars or $self->_error("Name can't be empty");
+    $attr->{name} = $self->var(@vars);
+    $self->tag('select', $self->select_options(@vars, $opts), $attr);
 }
 
 sub label {
-    my $attrs = ref $_[-1] eq 'HASH' ? pop @_ : {};
-    my ($self, @name) = @_;
-    $attrs->{for} //= join('.', @name) or
-        $self->{context}->throw("name missing");
-    $attrs->{for} = join '.', $self->var, $attrs->{for} if $self->var;
-    $attrs->{for} =~ m/(\w+)$/;
-    my $value = $1;
-    $self->tag('label', $attrs, $value);
+    my ($self, @args) = @_;
+    $self->tag('label', @args);
 }
 
 sub submit {
-    my $attrs = ref $_[-1] eq 'HASH' ? pop @_ : {};
-    my ($self, $value) = @_;
-    $attrs->{type} = "submit";
-    $attrs->{value} //= $value // "Submit";
-    $self->tag('input', $attrs);
+    my ($self, @args) = @_;
+    my $attr = ref $args[-1] eq 'HASH' ? pop @args : {};
+    $attr->{value} = $args[0] // "Submit";
+    $attr->{type} = "submit";
+    $self->tag('input', $attr);
 }
 
 sub tag {
-    my ($self, $tag, $attrs, $inside) = @_;
-    $attrs = $self->_join_attrs($attrs);
-    if (defined $inside) {
-        "<$tag$attrs>$inside</$tag>";
+    my ($self, $tag, @args) = @_;
+    my $attr = ref $args[-1] eq 'HASH' ? pop @args : {};
+    $attr = $self->_attributes($attr);
+    if (defined $args[0]) {
+        "<$tag$attr>$args[0]</$tag>";
     } else {
-        "<$tag$attrs>";
+        "<$tag$attr>";
     }
 }
 
-sub _join_attrs {
-    my ($self, $attrs) = @_;
-    join "", map qq( $_="$attrs->{$_}"), keys %$attrs;
+sub _attributes {
+    my $attr = $_[1]; join "", map qq( $_="$attr->{$_}"), keys %$attr;
+}
+
+sub _error {
+    $_[0]->{context}->throw($_[1]);
 }
 
 __PACKAGE__;
