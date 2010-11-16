b687c9ef1d699b8b049157bc761f5e40e02d12c2
Nicolas <nicolas.steenlant@ugent.be>
2010-11-16 10:47:43 +0100

    App::Request pod

--- lib/Catmandu/App/Request.pm
+++ lib/Catmandu/App/Request.pm
@@ -21,3 +21,45 @@ sub expand_param {
 
 __PACKAGE__;
 
+__END__
+
+=head1 NAME
+
+Catmandu::App::Request - class representing a HTTP request.
+
+=head1 DESCRIPTION
+
+This is subclass of L<Plack::Request>. You shouldn't create instances
+of this class explicitly. Instances are created on each HTTP request
+and passed to the L<Catmandu::App> instance handling the request.
+
+=head1 METHODS
+
+See L<Plack::Request> for the inherited methods.
+
+Extra methods for this class:
+
+=head2 $c->expand_param($key)
+
+Gives support for nested query params.
+Unflattens the request params that have a key starting with C<$key>
+and expands them into a deep hashref.
+
+Given a query string
+    "obj.foo=foo&foo.obj=foo&obj.bar.0=b&obj.baz.foo=bar&obj.bar.2=r"
+the key "obj" gets expanded to:
+    {
+        foo => "foo",
+        bar => ["b", undef, "r"],
+        baz => { foo => "bar" },
+    }
+
+See L<CGI::Expand> for details and more examples.
+
+=head1 SEE ALSO
+
+
+L<Plack::Request>, the superclass.
+
+L<CGI::Expand>.
+
