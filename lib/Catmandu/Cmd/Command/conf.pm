e2fe1b40d639bfe1c64c35eb721bb7a152779b92
Nicolas <nicolas.steenlant@ugent.be>
2010-12-22 15:38:38 +0100

    conf command

---
+++ lib/Catmandu/Cmd/Command/conf.pm
@@ -0,0 +1,37 @@
+package Catmandu::Cmd::Command::conf;
+# VERSION
+use namespace::autoclean;
+use Moose;
+use Catmandu;
+use Catmandu::Util;
+
+extends qw(Catmandu::Cmd::Command);
+
+with qw(
+    Catmandu::Cmd::Opts::Exporter
+);
+
+sub execute {
+    my ($self, $opts, $args) = @_;
+
+    $self->exporter =~ /::/ or $self->exporter("Catmandu::Exporter::" . $self->exporter);
+
+    if (my $arg = shift @$args) {
+        $self->exporter_arg->{file} = $arg;
+    }
+
+    Catmandu::Util::load_class($self->exporter);
+
+    my $exporter = $self->exporter->new($self->exporter_arg);
+
+    print $exporter->dump(Catmandu->conf);
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+=head1 NAME
+
+Catmandu::Cmd::Command::conf - export the project's configuration
+
