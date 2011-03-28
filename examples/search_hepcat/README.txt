7fd4e4905224b2254d8b7f80b3700cd8069796a1
Dave Sherohman <Dave.Sherohman@lub.lu.se>
2011-03-28 14:25:31 +0200

    Removed symlinks to Catmandu libs because git handles them poorly

--- examples/search_hepcat/README.txt
+++ examples/search_hepcat/README.txt
@@ -4,6 +4,11 @@ This example requires Dancer and Module::Pluggable to be installed.  All other
 dependencies should already be satisfied by the normal Catmandu installation
 process.
 
+If you copy this example to another directory and do not already have the
+Catmandu modules installed to a location in your @INC, you will need to
+modify the "use lib" statement in lib/HepCat/Model/Catmandu.pm so that it
+points to the location of your Catmandu modules.
+
 
 === Usage ===
 
--- examples/search_hepcat/lib/HepCat/Model/Catmandu.pm
+++ examples/search_hepcat/lib/HepCat/Model/Catmandu.pm
@@ -2,6 +2,17 @@ package HepCat::Model::Catmandu;
 
 use Dancer ':syntax';
 
+# Minor hack to find the Catmandu lib dir within this git installation; this
+# will have to be changed if you copy this example to another directory.
+#
+# It's handled this way because git has a tendency to duplicate the contents
+# of symlinked directories when applying changes rather than maintaining the
+# link as a link.  Note that if you have already installed Catmandu into your
+# @INC (e.g., installing it via CPAN), the FindBin lines are superflous and
+# are not required, but will not hurt anything, either.
+use FindBin;
+use lib "$FindBin::Bin/../../../lib";
+
 use Catmandu;
 use Catmandu::Util;
 
