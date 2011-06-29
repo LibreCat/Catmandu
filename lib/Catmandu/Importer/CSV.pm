861f4c7e24f9a78ec07ff00db614cbd8e6323003
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2011-06-29 10:12:12 -0400

    Importer::CSV fields param

--- lib/Catmandu/Importer/CSV.pm
+++ lib/Catmandu/Importer/CSV.pm
@@ -4,8 +4,29 @@ use Catmandu::Util qw(io);
 use Text::CSV;
 use Catmandu::Object
     file => { default => sub { *STDIN } },
+    fields => 'r',
     quote_char => { default => sub { '"' } },
     split_char => { default => sub { ',' } };
+
+sub _build {
+    my ($self, $args) = @_;
+    $self->SUPER::_build($args);
+    if ($self->fields) {
+        $self->_set_fields($self->fields);
+    }
+}
+
+sub _set_fields {
+    my ($self, $fields) = @_;
+
+    if (!ref $fields) {
+        [ split $self->split_char, $fields ];
+    } elsif (ref $fields eq 'HASH') {
+        [ keys %$fields ];
+    } elsif (ref $fields eq 'ARRAY') {
+        $fields;
+    }
+}
 
 sub each {
     my ($self, $sub) = @_;
@@ -18,7 +39,8 @@ sub each {
         sep_char   => $self->split_char,
     });
 
-    my $fields = $csv->getline($file);
+    my $fields = $self->fields ||
+                 $self->_set_fields($csv->getline($file));
 
     $csv->column_names($fields);
 
