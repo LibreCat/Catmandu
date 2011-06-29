697a9a17987ff48b92da4cca0ac706f2875eede5
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2011-06-29 10:48:30 -0400

    Importer::OAI record param

--- lib/Catmandu/Importer/OAI.pm
+++ lib/Catmandu/Importer/OAI.pm
@@ -1,6 +1,6 @@
 package Catmandu::Importer::OAI;
 use Catmandu::Sane;
-use Catmandu::Object url => 'r', set => 'r';
+use Catmandu::Object url => 'r', set => 'r', record => 'r';
 use Net::OAI::Harvester;
 
 my @oai_dc_elements = qw(
@@ -21,13 +21,30 @@ my @oai_dc_elements = qw(
     rights
 );
 
+sub _record_to_obj {
+    my ($self, $rec) = @_;
+    my $obj = { _id => $rec->header->identifier };
+    my $metadata = $rec->metadata;
+    foreach (@oai_dc_elements) {
+        $obj->{$_} = $metadata->{$_};
+    }
+    $obj;
+}
+
 sub each {
     my ($self, $sub) = @_;
 
     my $harvester = Net::OAI::Harvester->new(baseURL => $self->url);
 
+    if ($self->record) {
+        my $rec = $harvester->getRecord(metadataPrefix => 'oai_dc', identifier => $self->record);
+        my $obj = $self->_record_to_obj($rec);
+        $sub->($obj);
+        return 1;
+    }
+
     my $records = $harvester->listAllRecords(
-        metadataPrefix  => 'oai_dc',
+        metadataPrefix => 'oai_dc',
         set => $self->set,
     );
 
