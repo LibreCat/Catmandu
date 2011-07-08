ec03006a48b78d516952979038bc3c56aa7af078
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2011-07-08 04:53:40 -0400

    utf8 encode ris exports

--- lib/Catmandu/Exporter/RIS.pm
+++ lib/Catmandu/Exporter/RIS.pm
@@ -2,6 +2,7 @@ package Catmandu::Exporter::RIS;
 use Catmandu::Sane;
 use Catmandu::Util qw(io quack);
 use Catmandu::Object file => { default => sub { *STDOUT } };
+use Encode qw(encode_utf8);
 
 my $tags = [qw(TY ID T1 TI CT T2 BT T3 A1 AU A2 ED A3 Y1 PY Y2 N1 AB N2 KW RP 
                JF JO JA J1 J2 VL IS CP SP EP CY PB SN AD AV M1 M2 M3 U1 U2 U3 U4 U5
@@ -21,9 +22,8 @@ sub add {
             if (my $val = $o->{$tag}) {
                 $val = [$val] unless ref $val;
                 for my $v (@$val) {
-                    if (length($v) > 255 && $v =~ $small_tag) {
-                        $v = substr($v, 255);
-                    }
+                    $v = substr($v, 255) if length($v) > 255 && $v =~ $small_tag;
+                    $v = encode_utf8($v);
                     print $file "$tag  - $v\r\n";
                 }
             }
--- lib/Template/Plugin/UTF8.pm
+++ lib/Template/Plugin/UTF8.pm
@@ -1,21 +1,17 @@
 package Template::Plugin::UTF8;
 use parent qw(Template::Plugin::Filter);
-use Encode;
+use Encode ();
 
 my $FILTER_NAME = 'utf8';
 
 sub init {
-    my ($self) = @_;
-
+    my $self = $_[0];
     $self->install_filter($FILTER_NAME);
-
     $self;
 }
 
 sub filter {
-    my ($self, $text) = @_;
-
-    Encode::encode_utf8($text);
+    Encode::encode_utf8($_[1]);
 }
 
 1;
