a9cc4c0704d356e81f4f1d53d75aafb01a8d49ed
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2011-09-20 11:39:57 +0200

    Template exporter: xml option, detect Dancer

--- lib/Catmandu/Exporter/Template.pm
+++ lib/Catmandu/Exporter/Template.pm
@@ -1,23 +1,45 @@
 package Catmandu::Exporter::Template;
 use Catmandu::Sane;
 use Catmandu::Util qw(io quack);
-use Catmandu::Object file => { default => sub { *STDOUT } }, view => 'r';
 use Template;
+use Catmandu::Object
+    file => { default => sub { *STDOUT } },
+    view => 'r',
+    xml  => 'r';
 
 $Template::Stash::PRIVATE = 0;
+
+sub template_name {
+    my $name = $_[0];
+    $name = "$name.tt" if $name !~ /\.tt$/;
+    $name;
+}
 
 sub add {
     my ($self, $obj) = @_;
 
-    my $file = io $self->file, 'w';
-    my $view = $self->view;
-    my $tmpl = Template->new({
-            ENCODING => 'utf8',
-            ABSOLUTE => 1,
-    });
+    my $file = io($self->file, 'w');
 
-    if ($view !~ /\.tt$/) {
-        $view = "$view.tt";
+    my $view = template_name($self->view);
+
+    my $tmpl_opts = {
+        ENCODING => 'utf8',
+        ABSOLUTE => 1,
+        ANYCASE  => 0,
+    };
+
+    if ($ENV{DANCER_APPDIR}) {
+        require Dancer;
+        $tmpl_opts->{INCLUDE_PATH} = Dancer::setting('views');
+        $tmpl_opts->{VARIABLES} = {
+            settings => Dancer::Config->settings,
+        };
+    }
+
+    my $tmpl = Template->new($tmpl_opts);
+
+    if ($self->xml) {
+        print $file qq(<?xml version="1.0" encoding="UTF-8"?>\n);
     }
 
     if (quack $obj, 'each') {
