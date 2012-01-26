8e02592fff1bbb7c03e95dab622a2d1e291cf920
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2012-01-26 06:29:34 +0100

    Fixing rename field adding support to rename deep hashes/arrays.

--- lib/Catmandu/Fix/rename_field.pm
+++ lib/Catmandu/Fix/rename_field.pm
@@ -4,26 +4,36 @@ use Catmandu::Sane;
 use Catmandu::Util qw(:is data_at);
 use Moo;
 
-has path    => (is => 'ro', required => 1);
-has old_key => (is => 'ro', required => 1);
-has new_key => (is => 'ro', required => 1);
+has path  => (is => 'ro', required => 1);
+has path2 => (is => 'ro', required => 1);
+has key   => (is => 'ro', required => 1);
+has key2  => (is => 'ro', required => 1);
 
 around BUILDARGS => sub {
-    my ($orig, $class, $path, $new_key) = @_;
+    my ($orig, $class, $path, $path2) = @_;
     $path = [split /\./, $path];
-    my $old_key = pop @$path;
-    $orig->($class, path => $path, old_key => $old_key, new_key => $new_key);
+    my $key = pop @$path;
+    $path2 = [split /\./, $path2];
+    my $key2 = pop @$path2;
+    $orig->($class, path => $path, key => $key, path2 => $path2, key2 => $key2);
 };
 
 sub fix {
     my ($self, $data) = @_;
 
-    my $old_key = $self->old_key;
-    my $new_key = $self->new_key;
-    for my $match (data_at($self->path, $data)) {
-        if (is_array_ref($match) && exists $match->{$old_key}) {
-            $match->{$new_key} = delete $match->{$old_key};
+    my $key = $self->key;
+    my $key2 = $self->key2;
+
+    my @matches  = grep ref, data_at($self->path, $data, create => 1);
+    my $match2   = [grep ref, data_at($self->path2, $data, create => 1)]->[0];
+
+    for my $match (@matches) {
+        if (is_array_ref($match2)) {
+            $match2->[$key2] = is_array_ref($match) ? delete $match->[$key] : delete $match->{$key};
         }
+        else {
+	    $match2->{$key2} = is_array_ref($match) ? delete $match->[$key] : delete $match->{$key};
+	}
     }
 
     $data;
