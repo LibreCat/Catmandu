fc4fa2ec9a01eccb71d46bb0cb52384100fc272c
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2012-01-30 12:25:58 +0100

    improve Store DBI; Serializer

--- lib/Catmandu/Serializer.pm
+++ lib/Catmandu/Serializer.pm
@@ -9,29 +9,39 @@ use MIME::Base64 ();
 
 sub default_serialization_format { 'json' }
 
-has serialization_format => (is => 'ro');
+my $messagepack = Data::MessagePack->new->utf8;
+my $formats = {
+    storable => {
+        serialize   => sub { MIME::Base64::encode(Storable::nfreeze($_[0])) },
+        deserialize => sub { Storable::thaw(MIME::Base64::decode($_[0])) },
+    },
+    messagepack => {
+        serialize   => sub { MIME::Base64::encode($messagepack->pack($_[0])) },
+        deserialize => sub { $messagepack->unpack(MIME::Base64::decode($_[0])) },
+    },
+    json => {
+        serialize   => sub { JSON::encode_json($_[0]) },
+        deserialize => sub { JSON::decode_json($_[0]) },
+    },
+};
+
+has serialization_format => (is => 'ro', default => sub { '' });
 has serializer           => (is => 'ro', lazy => 1, builder => '_build_serializer');
 has deserializer         => (is => 'ro', lazy => 1, builder => '_build_deserializer');
 
 sub _build_serializer {
     my $self = $_[0];
-    my $fmt  = $self->serialization_format || $self->default_serialization_format;
-    return sub { MIME::Base64::encode(Storable::nfreeze($_[0])) } if $fmt eq 'storable';
-    return sub { MIME::Base64::encode($self->_messagepack->pack($_[0])) } if $fmt eq 'messagepack';
-    return sub { JSON::encode_json($_[0]) } if $fmt eq 'json';
-    confess "unknown serialization format";
+    my $format = $formats->{$self->serialization_format || $self->default_serialization_format}
+        or confess "unknown serialization format";
+    $format->{serializer};
 }
 
 sub _build_deserializer {
     my $self = $_[0];
-    my $fmt  = $self->serialization_format || $self->default_serialization_format;
-    return sub { Storable::thaw(MIME::Base64::decode($_[0])) } if $fmt eq 'storable';
-    return sub { $self->_messagepack->unpack(MIME::Base64::decode($_[0])) } if $fmt eq 'messagepack';
-    return sub { JSON::decode_json($_[0]) } if $fmt eq 'json';
-    confess "unknown serialization format";
+    my $format = $formats->{$self->serialization_format || $self->default_serialization_format}
+        or confess "unknown serialization format";
+    $format->{deserializer};
 }
-
-sub _messagepack { state $messagepack = Data::MessagePack->new->utf8 }
 
 sub serialize {
     $_[0]->serializer->($_[1]);
