5b64bfca9e597ed1d706d5792d3a00adb0df3aff
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2012-04-30 16:13:51 +0200

    smarter catmandu script

--- lib/Catmandu/CLI.pm
+++ lib/Catmandu/CLI.pm
@@ -3,19 +3,50 @@ use Catmandu::Sane;
 use App::Cmd::Setup -app;
 use Dancer qw(:syntax);
 use File::Spec;
-use Cwd qw(realpath);
-use FindBin;
-
-sub default_appdir { realpath(File::Spec->catdir($FindBin::Bin, '..')) }
+use Cwd ();
 
 sub plugin_search_path { 'Catmandu::Cmd' }
 
 sub global_opt_spec {
     (
-        ['environment=s', "application environment"],
-        ['appdir=s', "application directory (default is ".$_[0]->default_appdir.")"],
-        ['confdir=s', "application config directory (default is appdir)"],
+        ['environment=s', "app environment"],
+        ['appdir=s', "app directory (default is ".$_[0]->appdir.")"],
+        ['confdir=s', "app config directory (default is appdir)"],
     );
+}
+
+my $appdir;
+
+sub is_appdir {
+    my ($class, $dir) = @_;
+    -f File::Spec->catfile($dir, 'config.yml');
+}
+
+sub default_appdir {
+    my ($class) = @_;
+    # try script parent dir
+    my ($vol, $dir, $script) = File::Spec->splitpath($0);
+    $dir = Cwd::realpath(File::Spec->catdir($dir, File::Spec->updir));
+    return $dir if $class->is_appdir($dir);
+    # search for appdir upwards from cwd
+    my @dirs = File::Spec->splitdir(my $cwd = Cwd::getcwd);
+    do {
+        $dir = File::Spec->catdir(@dirs);
+        return $dir if $class->is_appdir($dir);
+    } while (defined pop @dirs);
+    # default to cwd
+    $cwd;
+}
+
+sub appdir {
+    $appdir //= $_[0]->default_appdir;
+}
+
+sub run_as_script {
+    my ($class) = @_;
+    my ($vol, $dir, $script) = File::Spec->splitpath($0);
+    $appdir = Cwd::realpath(File::Spec->catdir($dir, File::Spec->updir));
+    $class->run;
 }
 
 # overload run to read the global options before
@@ -23,9 +54,9 @@ sub global_opt_spec {
 sub run {
   my ($class) = @_;
 
-  my ($global_opts, $argv) = $class->_process_args(\@ARGV, $class->_global_option_processing_params);
+  my ($global_opts, $argv) = $class->_process_args([@ARGV], $class->_global_option_processing_params);
 
-  my $appdir = $global_opts->{appdir} || $ENV{DANCER_APPDIR} || $class->default_appdir;
+  my $appdir = $global_opts->{appdir} || $ENV{DANCER_APPDIR} || $class->appdir;
   my $libdir = File::Spec->catdir($appdir, 'lib');
 
   Dancer::setting(appdir => $appdir);
