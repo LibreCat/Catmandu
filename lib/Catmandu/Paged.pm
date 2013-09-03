03c21ff320c6eed7aaa076418c78c819a8080da1
vpeil <vitali.peil@uni-bielefeld.de>
2013-09-03 11:02:04 +0200

    subs completed, todo: pages_in_spread

--- lib/Catmandu/Paged.pm
+++ lib/Catmandu/Paged.pm
@@ -8,26 +8,6 @@ requires 'start';
 requires 'limit';
 requires 'total';
 
-# has max_pages_in_spread => (is => 'rw', lazy => 1, default => sub { 5 });
-
-# has _pager => (
-#     is => 'ro',
-#     init_arg => undef,
-#     lazy => 1,
-#     builder => '_build_pager',
-#     handles => {
-#         page => 'current_page',
-#         first_page => 'first_page',
-#         last_page => 'last_page',
-#         next_page => 'next_page',
-#         previous_page => 'previous_page',
-#         first_on_page => 'first',
-#         last_on_page => 'last',
-#         pages_in_spread => 'pages_in_spread',
-#         page_ranges => 'page_ranges',
-#     },
-# );
-
 sub first_page {
     my $self = shift;
 
@@ -51,8 +31,6 @@ sub last_page {
 sub current_page {
     my $self = shift;
 
-    #( $self->start <= $self->limit ) && ( return $self->first_page );
-    #( $self->start > ($self->total - $self->limit) ) && ( return $self->last_page );
     my $current = 1 + int ($self->start/$self->limit);
 }
 
@@ -77,11 +55,24 @@ sub first_on_page {
         : (return ( ( $self->current_page - 1 ) * $self->limit ) + 1 );
 }
 
-sub last {
+sub last_on_page {
     my $self = shift;
 
     ( $self->current_page == $self->last_page ) ? ( return $self->total_entries )
         : ( return ( $self->current_page * $self->limit ) );
+}
+
+sub page_ranges {
+    my $self = shift;
+
+    my @range = ($self->start, $self->start + $self->limit -1);
+    return \@range;
+}
+
+sub pages_in_spread {
+    my $self = shift;
+
+    return "I do not know what this means."
 }
 
 sub page_size {
