ceabe18617960362b3622d236178b499ef2221c4
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2013-12-18 14:52:48 +0100

    Exporter::CSV always_quote option

--- lib/Catmandu.pm
+++ lib/Catmandu.pm
@@ -37,11 +37,11 @@ To install all Catmandu components in one easy step:
 
 =head1 VERSION
 
-Version 0.8002
+Version 0.8004
 
 =cut
 
-our $VERSION = '0.8002';
+our $VERSION = '0.8004';
 
 =head1 SYNOPSIS
 
