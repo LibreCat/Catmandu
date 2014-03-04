55e5c431c02c09a908e4462f64099fee7d1299e2
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2014-03-04 11:02:30 +0100

    bump version

--- lib/Catmandu.pm
+++ lib/Catmandu.pm
@@ -37,11 +37,11 @@ To install all Catmandu components in one easy step:
 
 =head1 VERSION
 
-Version 0.8005
+Version 0.8006
 
 =cut
 
-our $VERSION = '0.8005';
+our $VERSION = '0.8006';
 
 =head1 SYNOPSIS
 
