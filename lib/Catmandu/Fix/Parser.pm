766daaac646abe75ce30e97e347ac610c676a71e
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2014-04-22 16:01:02 +0200

    working marpa parser

--- lib/Catmandu/Fix/Parser.pm
+++ lib/Catmandu/Fix/Parser.pm
@@ -75,6 +75,11 @@ sep ~ [,;:]
 GRAMMAR
 
 sub parse {
+    state $grammar = Marpa::R2::Scanless::G->new({
+        bless_package  => __PACKAGE__,
+        source => \$GRAMMAR,
+    });
+
     my ($self, $source) = @_;
 
     check_string($source);
@@ -83,16 +88,11 @@ sub parse {
         $source = read_file($source);
     }
 
-    my $grammar = Marpa::R2::Scanless::G->new({
-        bless_package  => __PACKAGE__,
-        source => \$GRAMMAR,
-    });
     my $recce = Marpa::R2::Scanless::R->new({grammar => $grammar});
     $recce->read(\$source);
 
-    use Data::Dumper;
     my $parsed = ${$recce->value};
-    die Dumper([map {$_->reify} @$parsed]);
+    [map {$_->reify} @$parsed];
 }
 
 sub Catmandu::Fix::Parser::IfElse::reify {
@@ -172,136 +172,6 @@ sub Catmandu::Fix::Parser::BareString::reify {
 sub Catmandu::Fix::Parser::Int::reify {
     int($_[0]->[0]);
 }
-
-
-#sub _parser {
-    #state $parser = do {
-        #use Regexp::Grammars;
-        #qr/
-            #<expr>
-
-            #<rule: expr>         <if_block>
-                                 #(?{ my $fix = $MATCH{if_block}{fix};
-                                     #my $instance = _build_fix_instance($fix->{name}, 'Catmandu::Fix::Condition', $fix->{args} || []);
-                                     #if ($MATCH{if_block}{expr}) {
-                                         #push @{$instance->fixes},
-                                            #map { $_->{instance} } @{$MATCH{if_block}{expr}};
-                                     #}
-                                     #if ($MATCH{if_block}{else_block} && $MATCH{if_block}{else_block}{expr}) {
-                                         #push @{$instance->else_fixes},
-                                            #map { $_->{instance} } @{$MATCH{if_block}{else_block}{expr}};
-                                     #}
-                                     #$MATCH{instance} = $instance;
-                                 #})
-                                 #|
-                                 #<unless_block>
-                                 #(?{ my $fix = $MATCH{unless_block}{fix};
-                                     #my $instance = _build_fix_instance($fix->{name}, 'Catmandu::Fix::Condition', $fix->{args} || []);
-                                     #if ($MATCH{unless_block}{expr}) {
-                                         #push @{$instance->else_fixes},
-                                            #map { $_->{instance} } @{$MATCH{unless_block}{expr}};
-                                     #}
-                                     #$MATCH{instance} = $instance;
-                                 #})
-                                 #|
-                                 #<select>
-                                 #(?{ my $fix = $MATCH{select}{fix};
-                                     #my $instance = _build_fix_instance($fix->{name}, 'Catmandu::Fix::Condition', $fix->{args} || []);
-                                     #push @{$instance->else_fixes}, Catmandu::Fix::Filter->new;
-                                     #$MATCH{instance} = $instance;
-                                 #})
-                                 #|
-                                 #<reject>
-                                 #(?{ my $fix = $MATCH{reject}{fix};
-                                     #my $instance = _build_fix_instance($fix->{name}, 'Catmandu::Fix::Condition', $fix->{args} || []);
-                                     #push @{$instance->fixes}, Catmandu::Fix::Filter->new;
-                                     #$MATCH{instance} = $instance;
-                                 #})
-                                 #|
-                                 #<fix>
-                                 #(?{ my $fix = $MATCH{fix};
-                                     #my $instance = _build_fix_instance($fix->{name}, 'Catmandu::Fix', $fix->{args} || []);
-                                     #$MATCH{instance} = $instance;
-                                 #})
-
-            #<rule: if_block>     if <fix> <[expr]>* <else_block>? end
-                                 #|
-                                 #if_<fix> <[expr]>* end \( \)
-
-            #<rule: else_block>   else <[expr]>*
-
-            #<rule: unless_block> unless <fix> <[expr]>* end
-                                 #|
-                                 #unless_<fix> <[expr]>* end \( \)
-
-            #<rule: select>       select \( <fix> \)
-            #<rule: reject>       reject \( <fix> \)
-
-            #<rule: fix>          <name> \( \)
-                                 #|
-                                 #<name> \( <args> \)
-
-            #<rule: args>         <[arg]>+ % <_sep>
-                                 #<MATCH= (?{ $MATCH{arg} })>
-
-            #<rule: arg>          <int>
-                                 #|
-                                 #<qq_string>
-                                 #|
-                                 #<q_string>
-                                 #|
-                                 #<string>
-                                 #|
-                                 #<fatal: Expected string or int>
-
-            #<token: keyword>     if|unless|end|select|reject
-
-            #<token: name>        <!keyword>
-                                 #[a-z][a-z0-9_-]*
-
-            #<token: int>         (-?\d+)
-                                 #<MATCH= (?{ eval $CAPTURE })>
-
-            #<token: qq_string>   "((?:[^\\"]|\\.)*)"
-                                 #<MATCH= (?{ $CAPTURE })>
-
-            #<token: q_string>    '((?:[^\\']|\\.)*)'
-                                 #<MATCH= (?{ $CAPTURE })>
-
-            #<token: string>      <!keyword>
-                                 #[^\s,;:=>\(\)"']+
-
-            #<token: _sep>        (?:\s|,|;|:|=>)+
-
-            #<token: ws>          (?:<_sep>)*
-        #/xms;
-    #};
-#}
-
-#sub parse {
-    #my ($self, $source) = @_;
-
-    #check_string($source);
-
-    #if ($source !~ /\(/) {
-        #$source = read_file($source);
-    #}
-
-    #$source =~ $self->_parser || do {
-        #my @err = @!;
-        #Catmandu::BadArg->throw(join("\n", "can't parse fix(es):", @err));
-    #};
-
-    #if (my $expr = $/{expr}) {
-        #if (is_array_ref($expr)) {
-            #[ map { $_->{instance} } @$expr ];
-        #} else {
-            #[ $expr->{instance} ];
-        #}
-    #} else {
-        #[];
-    #}
-#}
 
 1;
 
