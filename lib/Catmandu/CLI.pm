65d29750708fa8abd4d03e831180e7d0c7fd674e
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2014-05-17 18:29:42 +0200

    Moving all error processing enhancements to a new branch

--- lib/Catmandu/CLI.pm
+++ lib/Catmandu/CLI.pm
@@ -45,32 +45,7 @@ sub run {
     $self->set_global_options($global_opts);
     my ($cmd, $opts, @args) = $self->prepare_command(@$argv);
 
-    eval {
-        $self->execute_command($cmd, $opts, @args);
-    };
-    if ($@) {
-        if ($@ =~ /^Can't locate Catmandu\/Importer/ && $ARGV[1] eq 'help') {
-            print STDERR "You probably mean:\n\n\tcatmandu $ARGV[1] $ARGV[0]\n";
-            return undef;
-        }
-        elsif ($@ =~ /Can't locate Catmandu\/(Importer|Exporter|Store)\/([^\.]+)/) {
-            print STDERR "Can't find:\n\n\tCatmandu::$1\::$2\n\nin your installation. Or a\n\n\t$2\n\nin your catmandu.yml\n";
-            return undef;
-        }
-        elsif ($@ =~ /unknown store default/) {
-            print STDERR "You need to provide the type of a Store or define one in your catmandu.yml file.\n";
-            return undef;
-        }
-        elsif ($@ =~ /can't open "(.*)" for reading/) {
-            print STDERR "Can't find this Fix (file):\n\n\t$1\n\nDid you spell it correctly?\n";
-            return undef;
-        }
-        else {
-            die $@;
-        }
-    }
-    
-    1;
+    $self->execute_command($cmd, $opts, @args);
 }
 
 1;
