a2ee7024dcc2078ee637ec42365d98c5c8e998cd
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2014-06-03 11:48:44 +0200

    Added more pod to the pod

--- lib/Catmandu/Logger.pm
+++ lib/Catmandu/Logger.pm
@@ -16,14 +16,14 @@ sub _build_log {
 
 =head1 SYNOPSIS
 
-    package MyApp;
+    package MyApp::View;
     use Moo;
 
     with 'Catmandu::Logger';
 
     sub something {
         my ($self) = @_;
-        $self->log->debug("started bar"); # logs with default class catergory "MyApp"
+        $self->log->debug("started bar"); # logs with default class catergory "MyApp::View"
         $self->log->error("started bar");
     }
 
@@ -37,27 +37,54 @@ The logger needs to be setup before using the logger, which could happen in the 
 
     package main;
     use Log::Any::Adapter;
-    # Send all logs to Log::Log4perl
-    Log::Any::Adapter->set('Log4perl')
+    use Log::Log4perl;
 
+    Log::Any::Adapter->set('Log4perl');
+    Log::Log4perl::init('./log4perl.conf');
+
+    my $app = MyApp::View->new;
+    $app->something();  # will print debug and error messages
+
+with log4perl.conf as:
+
+    log4perl.rootLogger=DEBUG,STDOUT
+    log4perl.appender.STDOUT=Log::Log4perl::Appender::Screen
+    log4perl.appender.STDOUT.stderr=1
+    log4perl.appender.STDOUT.utf8=1
+
+    log4perl.appender.STDOUT.layout=PatternLayout
+    log4perl.appender.STDOUT.layout.ConversionPattern=%d [%P] - %p %l time=%r : %m%n
 
 =head1 ACCESSORS
 
 =head2 log
 
 The C<log> attribute holds the L<Log::Any::Adapter> object that implements all logging methods for the
-defined log levels, such as C<debug> or C<error>. As this method is defined also in other logging
-roles/systems like L<MooseX::Log::LogDispatch> this can be thought of as a common logging interface.
+defined log levels, such as C<debug> or C<error>. 
 
     package MyApp::View::JSON;
 
     extends 'MyApp::View';
-    with 'MooseX:Log::Log4perl';
+    with 'Catmandu::Logger';
 
     sub bar {
-        $self->logger->info("Everything fine so far");   # logs a info message
-        $self->logger->debug("Something is fishy here"); # logs a debug message
+        $self->log->info("Everything fine so far");   # logs a info message
+        $self->log->debug("Something is fishy here"); # logs a debug message
     }
+
+Your package automatically has a logging category of MyApp::View::JSON. Use lines like:
+
+    log4perl.logger.MyApp::View::JSON=DEBUG,STDOUT
+
+or
+
+    log4perl.logger.MyApp::View=DEBUG,STDOUT
+
+or
+
+    log4perl.logger.MyApp=DEBUG,STDOUT
+
+for specialized logging for your application.
 
 =head1 SEE ALSO
 
