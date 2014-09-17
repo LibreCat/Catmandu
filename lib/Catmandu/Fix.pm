36963f8ba4ffacf670053d53ddad12d1d78f302e
Jakob Voss <voss@gbv.de>
2014-09-17 14:05:31 +0200

    more POD

--- lib/Catmandu/Fix.pm
+++ lib/Catmandu/Fix.pm
@@ -670,7 +670,7 @@ E.g.
 
 Read more about the Fix language at our Wiki: L<https://github.com/LibreCat/Catmandu/wiki/Fixes>
 
-=head1 METHODS
+=head1 PUBLIC METHODS
 
 =head2 new(fixes => [ FIX , ...])
 
@@ -697,9 +697,7 @@ Executes all the fixes on a generator function. Returns a new generator with fix
 
 =head2 log
 
-Return the current logger. Can be used when creating your own Fix commands.
-
-E.g.
+Return the current logger. Can be used when creating your own Fix commands, e.g.
 
     package Catmandu::Fix::meow;
 
@@ -714,8 +712,56 @@ E.g.
         $data;
     }
 
-See also: L<Catmandu> for activating the logger in your main code.
+See L<Catmandu> for activating the logger in your main code.
 
+=head1 INTERNAL METHODS
+
+This module provides several methods for writing fix packages. Usage can best
+be understood by reading the code of existing fix packages.
+
+=over
+
+=item capture
+
+=item emit_block
+
+=item emit_clone
+
+=item emit_create_path
+
+=item emit_declare_vars
+
+=item emit_delete_key
+
+=item emit_fix
+
+=item emit_fixes
+
+=item emit_get_key
+
+=item emit_reject
+
+=item emit_retain_key
+
+=item emit_set_key
+
+=item emit_string
+
+=item emit_value
+
+=item emit_walk_path
+
+=item generate_var
+
+=item split_path
+
+=back
+
+=head1 SEE ALSO
+
+Fixes are used by instances of L<Catmandu::Fixable> to manipulate items
+L<Catmandu::Importer>, L<Catmandu::Exporter>, and L<Catmandu::Bag>.
+ 
 =cut
 
 1;
--- lib/Catmandu/Fixable.pm
+++ lib/Catmandu/Fixable.pm
@@ -20,9 +20,9 @@ Catmandu::Fixable - a Catmandu role to apply fixes
 
 =head1 DESCRIPTION
 
-This role provides a C<fix> attribute to apply L<Catmandu::Fix>es to items
-processed by L<Catmandu::Importer>, L<Catmandu::Exporter>, and
-L<Catmandu::Bag>.
+This role provides a C<fix> attribute to apply fixes (instances of
+L<Catmandu::Fix>) to items processed by L<Catmandu::Importer>,
+L<Catmandu::Exporter>, and L<Catmandu::Bag>.
  
 =cut
 
