60f84ea6565a8270249432241266829390242915
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2014-09-26 13:35:15 +0200

    Adding POD for Catmandu::Serializer::json

--- lib/Catmandu/Serializer/json.pm
+++ lib/Catmandu/Serializer/json.pm
@@ -12,4 +12,32 @@ sub deserialize {
     JSON::decode_json($_[1]);
 }
 
+=head1 NAME
+
+Catmandu::Serializer - A (de)serializer from and to json
+
+=head1 SYNOPSIS
+
+    package MyPackage;
+
+    use Moo;
+
+    with 'Catmandu::Serializer';
+    
+    # You have now  serialize and deserialize methods available
+
+    package main;
+
+    my $obj = MyPackage->new;
+    my $obj = MyPackage->new(serializer => 'json');
+
+    $obj->serialize( { foo => 'bar' } );  # JSON 
+    $obj->deserialize( "{'foo':'bar'}" );  # Perl
+
+=head1 SEE ALSO
+
+L<Catmandu::Serializer>
+
+=cut
+
 1;
