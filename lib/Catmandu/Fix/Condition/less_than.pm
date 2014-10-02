449072dc4e8f617febd1e9dfeea2677b6a11ad51
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2014-10-02 16:39:16 +0200

    Simplifying the greater_than less_than conditional

--- lib/Catmandu/Fix/Condition/less_than.pm
+++ lib/Catmandu/Fix/Condition/less_than.pm
@@ -1,35 +1,4 @@
 package Catmandu::Fix::Condition::less_than;
-use Catmandu::Sane;
-use Moo;
-use Catmandu::Fix::Has;
-
-with 'Catmandu::Fix::Condition';
-
-has path    => (fix_arg => 1);
-has value   => (fix_arg => 1);
-
-sub emit {
-    my ($self, $fixer, $label) = @_;
-    my $path = $fixer->split_path($self->path);
-    my $key = pop @$path;
-    my $str_key = $fixer->emit_string($key);
-    my $value = $fixer->emit_string($self->value);
-
-    my $perl = $fixer->emit_walk_path($fixer->var, $path, sub {
-        my $var  = shift;
-        $fixer->emit_get_key($var, $key, sub {
-            my $var = shift;
-            "if (is_value(${var}) && ${var} < $value) {" .
-            $fixer->emit_fixes($self->pass_fixes) .
-            "last $label;" .
-            "}";
-        });
-    });
-
-    $perl .= $fixer->emit_fixes($self->fail_fixes);
-
-    $perl;
-}
 
 =head1 NAME
 
@@ -39,7 +8,12 @@ Catmandu::Fix::Condition::less_than - Excute fixes when a field is less than a v
 
    # less_than(X,Y) is true when X < Y
    if less_than('year','2018')
-   	add_field('my.funny.title','true')
+    add_field('my.funny.title','true')
+   end
+
+   # less_than on arrays checks if all values are X < Y
+   if less_than('years.*','2018')
+     add_field('my.funny.title','true')
    end
 
 =head1 SEE ALSO
@@ -47,5 +21,20 @@ Catmandu::Fix::Condition::less_than - Excute fixes when a field is less than a v
 L<Catmandu::Fix::Condition::greater_than>
 
 =cut
+
+use Catmandu::Sane;
+use Moo;
+use Catmandu::Fix::Has;
+
+has path    => (fix_arg => 1);
+has value   => (fix_arg => 1);
+
+with 'Catmandu::Fix::Condition::SimpleAllTest';
+
+sub emit_test {
+    my ($self, $var) = @_;
+    my $value = $self->value;
+    "is_value(${var}) && ${var} < $value";
+}
 
 1;
