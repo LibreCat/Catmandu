d58adc4264491ac0c8015c08f999019791df6905
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2015-04-08 14:02:35 +0200

    Fix broken CSV exporter #135

--- lib/Catmandu/Exporter/CSV.pm
+++ lib/Catmandu/Exporter/CSV.pm
@@ -48,6 +48,7 @@ sub _build_csv {
 
 sub add {
     my ($self, $data) = @_;
+    return undef unless defined $data;
     $self->fields([ sort keys %$data ]) unless $self->fields;
     my $fields = $self->fields;
     my $row = [map {
