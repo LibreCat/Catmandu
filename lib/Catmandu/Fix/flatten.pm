0a538e6d0889505da3a56f8b3db85a24d0a7e976
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2015-09-07 16:47:50 +0200

    flatten fix

---
+++ lib/Catmandu/Fix/flatten.pm
@@ -0,0 +1,35 @@
+package Catmandu::Fix::flatten;
+
+use Catmandu::Sane;
+use Moo;
+use Catmandu::Fix::Has;
+
+has path => (fix_arg => 1);
+
+with 'Catmandu::Fix::SimpleGetValue';
+
+sub emit_value {
+    my ($self, $var) = @_;
+    "if (is_array_ref(${var})) {" .
+        "${var} = [map { ref \$_ eq 'ARRAY' ? \@\$_ : \$_ } \@{${var}}] " .
+        "while grep ref \$_ eq 'ARRAY', \@{${var}};" .
+    "}";
+}
+
+=head1 NAME
+
+Catmandu::Fix::flatten - flatten a nested array field
+
+=head1 SYNOPSIS
+
+   # {deep => [1, [2, 3], 4, [5, [6, 7]]]}
+   flatten(deep)
+   # {deep => [1, 2, 3, 4, 5, 6, 7]}
+
+=head1 SEE ALSO
+
+L<Catmandu::Fix>
+
+=cut
+
+1;
