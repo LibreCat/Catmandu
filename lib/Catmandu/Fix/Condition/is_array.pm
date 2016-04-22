70170c81e4be06ae9e4ad7dea07d6f1108aa0364
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2016-04-22 09:28:59 +0200

    is_array and is_null fix conditions

---
+++ lib/Catmandu/Fix/Condition/is_array.pm
@@ -0,0 +1,40 @@
+package Catmandu::Fix::Condition::is_array;
+
+use Catmandu::Sane;
+
+our $VERSION = '1.0002_01';
+
+use Moo;
+use namespace::clean;
+use Catmandu::Fix::Has;
+
+has path => (fix_arg => 1);
+
+with 'Catmandu::Fix::Condition::SimpleAllTest';
+
+sub emit_test {
+    my ($self, $var) = @_;
+    "is_array_ref(${var})";
+}
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Catmandu::Fix::Condition::is_array - only execute fixes if all path values are arrays
+
+=head1 SYNOPSIS
+
+   if is_array(data.*.list)
+     ...
+   end
+
+=head1 SEE ALSO
+
+L<Catmandu::Fix>
+
+=cut
---
+++ lib/Catmandu/Fix/Condition/is_null.pm
@@ -0,0 +1,40 @@
+package Catmandu::Fix::Condition::is_null;
+
+use Catmandu::Sane;
+
+our $VERSION = '1.0002_01';
+
+use Moo;
+use namespace::clean;
+use Catmandu::Fix::Has;
+
+has path => (fix_arg => 1);
+
+with 'Catmandu::Fix::Condition::SimpleAllTest';
+
+sub emit_test {
+    my ($self, $var) = @_;
+    "(defined(${var}) ? 0 : 1)";
+}
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Catmandu::Fix::Condition::is_null - only execute fixes if all path values are null
+
+=head1 SYNOPSIS
+
+   if is_null(data.*)
+     ...
+   end
+
+=head1 SEE ALSO
+
+L<Catmandu::Fix>
+
+=cut
