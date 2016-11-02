18f9b9d587217d8699d990aadb8732ad077b7433
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2016-11-02 14:03:14 +0100

    Adding more POD

--- lib/Catmandu/Fix/move_field.pm
+++ lib/Catmandu/Fix/move_field.pm
@@ -79,8 +79,27 @@ Catmandu::Fix::move_field - move a field to another place in the data structure
 
 =head1 SYNOPSIS
 
+   # Move single fields
+
    # Move 'foo.bar' to 'bar.foo'
    move_field(foo.bar, bar.foo)
+
+   # Move multipe fields
+   # Data:
+   # a:
+   #   b: test1
+   #   c: test2
+   move_field(a,z)  # -> Move all the 'a' to 'z'
+                    # z:
+                    #   b: test1
+                    #   c: test2
+   # Data:
+   # a:
+   #   b: test1
+   #   c: test2
+   move_field(a,.)  # -> Move the fields 'b' and 'c' to the root
+                    # b: test1
+                    # c: test2
 
 =head1 SEE ALSO
 
