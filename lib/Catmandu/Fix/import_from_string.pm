800315dbb5a35837c182e3c5e92a052f146dfaf7
Nicolas Franck <njfranck@ca20c533.ugent.be>
2017-03-16 10:12:16 +0100

    finish

--- lib/Catmandu/Fix/import_from_string.pm
+++ lib/Catmandu/Fix/import_from_string.pm
@@ -10,8 +10,8 @@ use namespace::clean;
 use Catmandu::Fix::Has;
 
 has path => (fix_arg => 1);
-has name => (fix_arg => 1,required => 0);
-has import_opts => (fix_arg => 'collect')
+has name => (fix_arg => 1);
+has import_opts => (fix_opt => 'collect');
 
 with 'Catmandu::Fix::SimpleGetValue';
 
@@ -32,12 +32,48 @@ __END__
 
 =head1 NAME
 
-Catmandu::Fix::import_from_string - Import data from a string into an array ref, using a default or named importer.
+Catmandu::Fix::import_from_string - Import data from a string into an array ref, using a named importer.
 
 =head1 SYNOPSIS
 
-    #{ 'json' => '[{"name":"Nicolas"}]' } => { 'json' => [{"name":"Nicolas"}] }
+    #BEFORE: { 'json' => '[{"name":"Nicolas"}]' }
+    #AFTER:  { 'json' => [{"name":"Nicolas"}] }
+
     import_from_string('json','JSON')
+
+    #BEFORE: { record => qq(first_name;name\nNicolas;Franck\nPatrick;Hochstenbach\n) }
+    #AFTER:  { record => [{ "first_name" => "Nicolas",name => "Franck" },{ "first_name" => "Patrick",name => "Hochstenbach" }] }
+
+    import_from_string('record','CSV', 'sep_char' => ';')
+
+
+=head1 DESCRIPTION
+
+=head2 import_from_string( PATH, NAME [, IMPORT_OPTIONS ] )
+
+This fix uses the function import_from_string of the package L<Catmandu>, but requires the NAME of the importer.
+
+It always returns an array of hashes.
+
+=over 4
+
+=item PATH
+
+=item NAME
+
+name of the importer to use. As usual in Catmandu, one can choose:
+
+* full package name of the importer (e.g. 'Catmandu::Importer::JSON')
+
+* short package name of the importer (e.g. 'JSON')
+
+* name of the importer as declared in the Catmandu configuration
+
+=item IMPORT_OPTIONS
+
+extra options for the named importer
+
+=back
 
 =head1 AUTHOR
 
---
+++ t/Catmandu-Fix-import_from_string.t
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Test::More;
+use Test::Exception;
+
+my $pkg;
+
+BEGIN {
+    $pkg = 'Catmandu::Fix::import_from_string';
+    use_ok $pkg;
+}
+
+is_deeply $pkg->new('record','JSON')->fix({ record => qq({"name":"Nicolas"}\n) }),
+    { record => [{ "name" => "Nicolas" }] }, "convert single JSON object to array of hashes";
+
+is_deeply $pkg->new('record','JSON')->fix({ record => qq([{"name":"Nicolas"}]\n) }),
+    { record => [{ "name" => "Nicolas" }] }, "convert JSON array to array of hashes";
+
+is_deeply $pkg->new('record','YAML')->fix({ record => qq(---\nname: Nicolas\n...\n) }),
+    { record => [{ "name" => "Nicolas" }] }, "convert single YAML object to array of hashes";
+
+is_deeply $pkg->new('record','YAML')->fix({ record => qq(---\nname: Nicolas\n...\n---\nname: Patrick\n...\n) }),
+    { record => [{ "name" => "Nicolas" },{ "name" => "Patrick" }] }, "convert YAML array to array of hashes";
+
+is_deeply $pkg->new('record','CSV')->fix({ record => qq(name\nNicolas\n) }),
+    { record => [{ "name" => "Nicolas" }] }, "convert single CSV line to array of hashes";
+
+is_deeply $pkg->new('record','CSV', sep_char => ';')->fix({ record => qq(first_name;name\nNicolas;Franck\nPatrick;Hochstenbach\n) }),
+    { record => [{ "first_name" => "Nicolas",name => "Franck" },{ "first_name" => "Patrick",name => "Hochstenbach" }] }, "convert CSV array to array of hashes";
+
+done_testing;
