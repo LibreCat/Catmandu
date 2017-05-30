459d3eefaba22b388960612e37c86f4e8ad61466
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2017-05-30 15:18:16 +0200

    use Data::Util again

--- lib/Catmandu/Util.pm
+++ lib/Catmandu/Util.pm
@@ -8,7 +8,7 @@ use Exporter qw(import);
 use Sub::Quote    ();
 use Scalar::Util  ();
 use List::Util    ();
-use Ref::Util     ();
+use Data::Util    ();
 use Data::Compare ();
 use IO::File;
 use IO::Handle::Util ();
@@ -398,8 +398,6 @@ sub array_split {
     is_array_ref($arr) ? $arr : [split ',', $arr];
 }
 
-*hash_merge = \&Hash::Merge::Simple::merge;
-
 sub as_utf8 {
     my $str = $_[0];
     utf8::upgrade($str);
@@ -421,8 +419,6 @@ sub capitalize {
     ucfirst lc $str;
 }
 
-*is_same = \&Data::Compare::Compare;
-
 sub is_different {
     !is_same(@_);
 }
@@ -436,104 +432,6 @@ sub check_different {
     is_same(@_) && Catmandu::BadVal->throw('should be different');
     $_[0];
 }
-
-# the following code is taken from Data::Util::PurePerl 0.63
-sub _get_stash {
-    my ($inv) = @_;
-
-    if (Scalar::Util::blessed($inv)) {
-        no strict 'refs';
-        return \%{ref($inv) . '::'};
-    }
-    elsif (!is_string($inv)) {
-        return undef;
-    }
-
-    $inv =~ s/^:://;
-
-    my $pack = *main::;
-    for my $part (split /::/, $inv) {
-        return undef unless $pack = $pack->{$part . '::'};
-    }
-    return *{$pack}{HASH};
-}
-
-sub _get_code_ref {
-    my ($pkg, $name, @flags) = @_;
-
-    is_string($pkg)  || Catmandu::BadVal->throw('should be string');
-    is_string($name) || Catmandu::BadVal->throw('should be string');
-
-    my $stash = _get_stash($pkg) or return undef;
-
-    if (defined(my $glob = $stash->{$name})) {
-        if (is_glob_ref(\$glob)) {
-            return *{$glob}{CODE};
-        }
-        else {    # a stub or special constant
-            no strict 'refs';
-            return *{$pkg . '::' . $name}{CODE};
-        }
-    }
-    return undef;
-}
-
-sub is_invocant {
-    my ($inv) = @_;
-    if (ref $inv) {
-        return !!Scalar::Util::blessed($inv);
-    }
-    else {
-        return !!_get_stash($inv);
-    }
-}
-
-*is_scalar_ref = \&Ref::Util::is_plain_scalarref;
-
-*is_array_ref = \&Ref::Util::is_plain_arrayref;
-
-*is_hash_ref = \&Ref::Util::is_plain_hashref;
-
-*is_code_ref = \&Ref::Util::is_plain_coderef;
-
-*is_regex_ref = \&Ref::Util::is_regexpref;
-
-*is_glob_ref = \&Ref::Util::is_plain_globref;
-
-sub is_value {
-    defined($_[0]) && !is_ref($_[0]) && !is_glob_ref(\$_[0]);
-}
-
-sub is_string {
-    is_value($_[0]) && length($_[0]) > 0;
-}
-
-sub is_number {
-    return 0 if !defined($_[0]) || is_ref($_[0]);
-
-    return $_[0] =~ m{
-        \A \s*
-                [+-]?
-                (?= \d | \.\d)
-                \d*
-                (\.\d*)?
-                (?: [Ee] (?: [+-]? \d+) )?
-        \s* \z
-    }xms;
-}
-
-sub is_integer {
-    return 0 if !defined($_[0]) || is_ref($_[0]);
-
-    return $_[0] =~ m{
-        \A \s*
-                [+-]?
-                \d+
-        \s* \z
-    }xms;
-}
-
-# end of code taken from Data::Util
 
 sub is_bool {
     Scalar::Util::blessed($_[0])
@@ -552,7 +450,9 @@ sub is_positive {
     is_integer($_[0]) && $_[0] >= 1;
 }
 
-*is_ref = \&Ref::Util::is_ref;
+sub is_ref {
+    ref $_[0] ? 1 : 0;
+}
 
 sub is_able {
     my $obj = shift;
@@ -595,28 +495,50 @@ sub check_maybe_instance {
         'should be undef or instance of ' . array_to_sentence(\@_));
 }
 
+Data::Util::install_subroutine(__PACKAGE__,
+    hash_merge => \&Hash::Merge::Simple::merge,
+    is_same => \&Data::Compare::Compare,
+    is_invocant => \&Data::Util::is_invocant,
+    is_scalar_ref => \&Data::Util::is_scalar_ref,
+    is_array_ref => \&Data::Util::is_array_ref,
+    is_hash_ref => \&Data::Util::is_hash_ref,
+    is_code_ref => \&Data::Util::is_code_ref,
+    is_regex_ref => \&Data::Util::is_rx,
+    is_glob_ref => \&Data::Util::is_glob_ref,
+    is_value => \&Data::Util::is_value,
+    is_string => \&Data::Util::is_string,
+    is_number => \&Data::Util::is_number,
+    is_integer => \&Data::Util::is_integer,
+);
+
 for my $sym (
     qw(able instance invocant ref
     scalar_ref array_ref hash_ref code_ref regex_ref glob_ref
     bool value string number integer natural positive)
     )
 {
-    my $pkg      = __PACKAGE__;
     my $err_name = $sym;
     $err_name =~ s/_/ /;
+
     push @EXPORT_OK, "is_$sym", "is_maybe_$sym", "check_$sym",
         "check_maybe_$sym";
     push @{$EXPORT_TAGS{is}},    "is_$sym",    "is_maybe_$sym";
     push @{$EXPORT_TAGS{check}}, "check_$sym", "check_maybe_$sym";
-    Sub::Quote::quote_sub("${pkg}::is_maybe_$sym",
-        "!defined(\$_[0]) || ${pkg}::is_$sym(\$_[0])")
-        unless _get_code_ref($pkg, "is_maybe_$sym");
-    Sub::Quote::quote_sub("${pkg}::check_$sym",
-        "${pkg}::is_$sym(\$_[0]) || Catmandu::BadVal->throw('should be $err_name'); \$_[0]"
-    ) unless _get_code_ref($pkg, "check_$sym");
-    Sub::Quote::quote_sub("${pkg}::check_maybe_$sym",
-        "${pkg}::is_maybe_$sym(\$_[0]) || Catmandu::BadVal->throw('should be undef or $err_name'); \$_[0]"
-    ) unless _get_code_ref($pkg, "check_maybe_$sym");
+
+    unless (Data::Util::get_code_ref(__PACKAGE__, "is_maybe_$sym")) {
+        my $sub = Sub::Quote::quote_sub("!defined(\$_[0]) || is_$sym(\$_[0])");
+        Data::Util::install_subroutine(__PACKAGE__, "is_maybe_$sym" => $sub);
+    }
+
+    unless (Data::Util::get_code_ref(__PACKAGE__, "check_$sym")) {
+        my $sub = Sub::Quote::quote_sub("is_$sym(\$_[0]) || Catmandu::BadVal->throw('should be $err_name'); \$_[0]");
+        Data::Util::install_subroutine(__PACKAGE__, "check_$sym" => $sub);
+    }
+
+    unless (Data::Util::get_code_ref(__PACKAGE__, "check_maybe_$sym")) {
+        my $sub = Sub::Quote::quote_sub("is_maybe_$sym(\$_[0]) || Catmandu::BadVal->throw('should be undef or $err_name'); \$_[0]");
+        Data::Util::install_subroutine(__PACKAGE__, "check_maybe_$sym" => $sub);
+    }
 }
 
 sub human_number {    # taken from Number::Format
