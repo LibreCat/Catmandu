e5082a0ab5cf27fe812669549161b49231469c79
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2017-10-20 09:35:38 +0200

    import fix builder version

--- lib/Catmandu/Fix/import.pm
+++ lib/Catmandu/Fix/import.pm
@@ -5,11 +5,12 @@ use Catmandu::Sane;
 our $VERSION = '1.0606';
 
 use Catmandu;
+use Catmandu::Util qw(as_path);
 use Moo;
 use namespace::clean;
 use Catmandu::Fix::Has;
 
-with 'Catmandu::Fix::Base';
+with 'Catmandu::Fix::Builder';
 
 has path       => (fix_arg => 1);
 has name       => (fix_arg => 1);
@@ -17,53 +18,29 @@ has delete     => (fix_opt => 1);
 has ignore_404 => (fix_opt => 1);
 has opts       => (fix_opt => 'collect');
 
-sub emit {
-    my ($self, $fixer) = @_;
-    my $path     = $fixer->split_path($self->path);
-    my $key      = pop @$path;
-    my $name_var = $fixer->capture($self->name);
-    my $opts_var = $fixer->capture($self->opts);
-    my $temp_var = $fixer->generate_var;
-
-    $fixer->emit_walk_path(
-        $fixer->var,
-        $path,
+sub _build_fixer {
+    my ($self)      = @_;
+    my $path        = as_path($self->path);
+    my $name       = $self->name;
+    my $opts       = $self->opts;
+    my $delete     = $self->delete;
+    my $ignore_404 = $self->ignore_404;
+    $path->updater(
         sub {
-            my $var = shift;
-            $fixer->emit_get_key(
-                $var, $key,
-                sub {
-                    my $val_var   = shift;
-                    my $index_var = shift;
-                    my $perl      = $fixer->emit_declare_vars($temp_var);
-                    if ($self->ignore_404) {
-                        $perl .= "try {";
-                    }
-                    $perl
-                        .= "${temp_var} = Catmandu->importer(${name_var}, variables => ${val_var}, %{${opts_var}})->first;";
-                    if ($self->ignore_404) {
-                        $perl
-                            .= "} catch_case ['Catmandu::HTTPError' => sub {"
-                            . "if (\$_->code eq '404') { ${temp_var} = undef; } else { \$_->throw }"
-                            . "}];";
-                    }
-                    $perl .= "if (defined(${temp_var})) {";
-                    $perl .= "${val_var} = ${temp_var};";
-                    $perl .= "}";
-                    if ($self->delete) {
-                        $perl .= "else {";
-                        if (defined $index_var)
-                        { # wildcard: only delete the value where the get failed
-                            $perl .= "splice(\@{${var}}, ${index_var}--, 1);";
-                        }
-                        else {
-                            $perl .= $fixer->emit_delete_key($var, $key);
-                        }
-                        $perl .= "}";
-                    }
-                    $perl;
+            my $val = $_[0];
+            try {
+                $val = Catmandu->importer($name, variables => $val, %$opts)
+                    ->first;
+            }
+            catch_case [
+                'Catmandu::HTTPError' => sub {
+                    if ($_->code eq '404' && $ignore_404) {$val = undef}
+                    else                                  {$_->throw}
                 }
-            );
+            ];
+
+            return $val if defined $val;
+            return undef, 1, $delete;
         }
     );
 }
